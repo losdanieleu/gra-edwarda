2da511129c04af8c10c117b953444c4aca7cdd56
losdanieleu <56912899+losdanieleu@users.noreply.github.com>
2022-01-25 12:50:41 +0100

    Update po ocenie styczeń

---
+++ Assets/CollectObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectObject : MonoBehaviour
+{
+    private object thisObject;
+
+    private void Awake()
+    {
+        thisObject = GetComponent<Object>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("PlayerCollider"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
